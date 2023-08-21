2c5642055a1fccdd35c53320b703c52e6e2911d6
tkylesh <taylorshuff90@gmail.com>
2023-08-21 12:03:46 -0500

    adding potteryWheel module to project with makePottery function.

---
+++ src/scripts/PotteryWheel.js
@@ -0,0 +1,13 @@
+let potteryId = 1
+
+
+export const makePottery = (shape, weight, height) => {
+    let newPottery = {
+        id: potteryId,
+        shape: shape,
+        weight: weight,
+        height: height
+    }
+    potteryId++
+        return newPottery
+}
